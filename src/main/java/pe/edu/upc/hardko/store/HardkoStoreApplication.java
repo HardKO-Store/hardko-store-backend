915cfd2dd405a4d6719d3d917ac16f665a466473
NeoRise456 <neorise456@gmail.com>
2024-11-04 17:12:14 -0500

    feat(products): added mongo annotations.

--- src/main/java/pe/edu/upc/hardko/store/HardkoStoreApplication.java
+++ src/main/java/pe/edu/upc/hardko/store/HardkoStoreApplication.java
@@ -2,10 +2,12 @@ package pe.edu.upc.hardko.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableMongoRepositories
+@EnableMongoAuditing
 public class HardkoStoreApplication {
 
 	public static void main(String[] args) {
