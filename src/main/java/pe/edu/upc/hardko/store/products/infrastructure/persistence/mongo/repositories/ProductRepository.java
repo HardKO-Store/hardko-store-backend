d8f25f4662da1e509d60d70f635c23668717dc7f
NeoRise456 <neorise456@gmail.com>
2024-11-04 18:03:29 -0500

    feat(products): add repository annotation to product repository.

--- src/main/java/pe/edu/upc/hardko/store/products/infrastructure/persistence/mongo/repositories/ProductRepository.java
+++ src/main/java/pe/edu/upc/hardko/store/products/infrastructure/persistence/mongo/repositories/ProductRepository.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.hardko.store.products.infrastructure.persistence.mongo.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.hardko.store.products.domain.model.aggregates.Product;
 
+@Repository
 public interface ProductRepository extends MongoRepository<Product, Long> {
 }
