21699eac4bf409151e3abaa22ce8f372cea2b716
NeoRise456 <neorise456@gmail.com>
2024-11-05 02:48:24 -0500

    feat(users): added user repository.

--- src/main/java/pe/edu/upc/hardko/store/IAM/infrastructure/persistence/mongo/repositories/UserRepository.java
+++ src/main/java/pe/edu/upc/hardko/store/IAM/infrastructure/persistence/mongo/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package pe.edu.upc.hardko.store.IAM.infrastructure.persistence.mongo.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import pe.edu.upc.hardko.store.IAM.domain.model.aggregates.User;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 }
