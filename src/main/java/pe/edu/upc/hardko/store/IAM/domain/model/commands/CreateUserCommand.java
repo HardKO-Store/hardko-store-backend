765ceb6d35c7de1526e49589e6b6b821247815e9
NeoRise456 <neorise456@gmail.com>
2024-11-05 02:45:49 -0500

    feat(users): updated create user command.

--- src/main/java/pe/edu/upc/hardko/store/IAM/domain/model/commands/CreateUserCommand.java
+++ src/main/java/pe/edu/upc/hardko/store/IAM/domain/model/commands/CreateUserCommand.java
@@ -1,4 +1,13 @@
 package pe.edu.upc.hardko.store.IAM.domain.model.commands;
 
-public record CreateUserCommand() {
+public record CreateUserCommand(
+        String firstName,
+        String lastName,
+        String email,
+        String password,
+        String country,
+        String city,
+        String street,
+        String zip
+) {
 }
