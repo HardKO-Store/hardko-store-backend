975168fa823d442bf6879bff91e88d91f63427df
NeoRise456 <neorise456@gmail.com>
2024-11-05 02:46:22 -0500

    feat(users): added user constructor for create user command.

--- src/main/java/pe/edu/upc/hardko/store/IAM/domain/model/aggregates/User.java
+++ src/main/java/pe/edu/upc/hardko/store/IAM/domain/model/aggregates/User.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.lang.NonNull;
+import pe.edu.upc.hardko.store.IAM.domain.model.commands.CreateUserCommand;
 import pe.edu.upc.hardko.store.IAM.domain.model.entities.UserAddress;
 import pe.edu.upc.hardko.store.shared.domain.model.entities.AuditableModel;
 
@@ -32,8 +33,23 @@ public class User extends AuditableModel {
     @NonNull
     private UserAddress address;
 
+    //TODO: implement user favorite products
+
     //TODO: implement order history
 
+
+    public User(CreateUserCommand command) {
+        this.firstName = command.firstName();
+        this.lastName = command.lastName();
+        this.email = command.email();
+        this.password = command.password();
+        this.address = new UserAddress(
+                command.country(),
+                command.city(),
+                command.street(),
+                command.zip()
+        );
+    }
 
 
 
