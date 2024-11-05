e4ab87723aa3fb4fd61ada1468dca53655743af8
NeoRise456 <neorise456@gmail.com>
2024-11-05 01:43:16 -0500

    refactor(products): added aggregation and validation queries to repository.

--- src/main/java/pe/edu/upc/hardko/store/products/infrastructure/persistence/mongo/repositories/ProductRepository.java
+++ src/main/java/pe/edu/upc/hardko/store/products/infrastructure/persistence/mongo/repositories/ProductRepository.java
@@ -1,10 +1,22 @@
 package pe.edu.upc.hardko.store.products.infrastructure.persistence.mongo.repositories;
 
 
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.hardko.store.products.domain.model.aggregates.Product;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
+
+    boolean existsById(String productId);
+    List<Product> findByCategoriesContaining(String category);
+
+    @Aggregation(pipeline = {
+            "{ $unwind: '$categories' }",
+            "{ $group: { _id: '$categories' } }"
+    })
+    List<String> findDistinctCategories();
 }
