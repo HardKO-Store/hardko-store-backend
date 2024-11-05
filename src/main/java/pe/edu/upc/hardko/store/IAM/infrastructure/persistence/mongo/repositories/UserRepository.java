c13cc52b7f36f7327037f00c5d8f8c6b702285a6
NeoRise456 <neorise456@gmail.com>
2024-11-05 15:05:30 -0500

    feat(users): added repository queries for login.

--- src/main/java/pe/edu/upc/hardko/store/IAM/infrastructure/persistence/mongo/repositories/UserRepository.java
+++ src/main/java/pe/edu/upc/hardko/store/IAM/infrastructure/persistence/mongo/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.hardko.store.IAM.domain.model.aggregates.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+    boolean existsByEmailAndPassword(String email, String password);
+    Optional<User> findByEmailAndPassword (String email, String password);
 }
