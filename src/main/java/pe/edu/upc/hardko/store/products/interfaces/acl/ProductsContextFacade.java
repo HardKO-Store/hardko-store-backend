b231231d1b6f00d75c6d56fef418260eec44edd0
NeoRise456 <neorise456@gmail.com>
2024-11-06 00:33:06 -0500

    feat(products): add fetchProductsByTheirIds function to product facade.

--- src/main/java/pe/edu/upc/hardko/store/products/interfaces/acl/ProductsContextFacade.java
+++ src/main/java/pe/edu/upc/hardko/store/products/interfaces/acl/ProductsContextFacade.java
@@ -1,9 +1,17 @@
 package pe.edu.upc.hardko.store.products.interfaces.acl;
 
 import org.springframework.stereotype.Service;
+import pe.edu.upc.hardko.store.products.domain.model.aggregates.Product;
 import pe.edu.upc.hardko.store.products.domain.model.queries.GetProductByIdQuery;
+import pe.edu.upc.hardko.store.products.domain.model.queries.GetProductsByTheirIdsQuery;
 import pe.edu.upc.hardko.store.products.domain.services.ProductCommandService;
 import pe.edu.upc.hardko.store.products.domain.services.ProductQueryService;
+import pe.edu.upc.hardko.store.products.interfaces.rest.resources.ProductResource;
+import pe.edu.upc.hardko.store.products.interfaces.rest.transform.ProductResourceFromEntityAssembler;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductsContextFacade {
@@ -22,6 +30,30 @@ public class ProductsContextFacade {
         var optionalProduct = this.productQueryService.handle(getProductByIdQuery);
 
         return optionalProduct.isPresent();
+    }
+
+    public Optional<ProductResource> fetchProductById(String productId) {
+        var getProductByIdQuery = new GetProductByIdQuery(productId);
+        var optionalProduct = this.productQueryService.handle(getProductByIdQuery);
+
+        if (optionalProduct.isEmpty()) {
+            return Optional.empty();
+        }
+
+        var productResource = ProductResourceFromEntityAssembler.toResourceFromEntity(optionalProduct.get());
+
+        return Optional.of(productResource);
+    }
+
+    public List<ProductResource> fetchProductsByTheirIds(List<String> productsIds){
+        var getProductsByTheirIdsQuery = new GetProductsByTheirIdsQuery(productsIds);
+        var products = this.productQueryService.handle(getProductsByTheirIdsQuery);
+
+        var productResources = products.stream()
+                .map(ProductResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+
+        return productResources;
     }
 
 
