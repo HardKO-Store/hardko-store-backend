fdfd461cff38a8414f7c83a6d47b09df7d5a9d9f
NeoRise456 <neorise456@gmail.com>
2024-11-06 00:33:26 -0500

    feat(users): add fetchProductsByTheirIds function to external product service.

--- src/main/java/pe/edu/upc/hardko/store/IAM/application/internal/outboundservices/acl/ExternalProductService.java
+++ src/main/java/pe/edu/upc/hardko/store/IAM/application/internal/outboundservices/acl/ExternalProductService.java
@@ -1,7 +1,11 @@
 package pe.edu.upc.hardko.store.IAM.application.internal.outboundservices.acl;
 
 import org.springframework.stereotype.Service;
+import pe.edu.upc.hardko.store.IAM.interfaces.rest.resoruces.FavoriteProductResource;
 import pe.edu.upc.hardko.store.products.interfaces.acl.ProductsContextFacade;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ExternalProductService {
@@ -15,5 +19,25 @@ public class ExternalProductService {
     public boolean existsProductById(String productId) {
         return this.productsContextFacade.existsProductById(productId);
     }
+
+    public List<FavoriteProductResource> fetchProductsByTheirIds(List<String> productsIds){
+
+        var productsResources = this.productsContextFacade.fetchProductsByTheirIds(productsIds);
+
+        var favoriteProductResources =  productsResources.stream()
+                .map(product -> new FavoriteProductResource(
+                        product.id(),
+                        product.name(),
+                        product.description(),
+                        product.categories(),
+                        product.price(),
+                        product.brand(),
+                        product.imageurl())
+                )
+                .collect(Collectors.toList());
+
+        return favoriteProductResources;
+    }
+
 
 }
