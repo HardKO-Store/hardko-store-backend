8a791473786c9f3c429d83006796c4a86cc029ee
NeoRise456 <neorise456@gmail.com>
2024-11-07 13:42:21 -0500

    feat(reviews): add repository functions.

--- src/main/java/pe/edu/upc/hardko/store/reviews/infrastructure/persistence/mongo/repositories/ReviewRepository.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/infrastructure/persistence/mongo/repositories/ReviewRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.hardko.store.reviews.domain.model.aggregates.Review;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
+    boolean existsReviewBy(String id);
+    List<Review> findReviewsByProductId (String productId);
+    List<Review> findReviewsByUserId(String userId);
 }
