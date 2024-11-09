434743744d2b351b96346c6b3cbf708af640cdf6
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:11:19 -0500

    fix(reviews): changed datatypes of parameters of functions of the repository.

--- src/main/java/pe/edu/upc/hardko/store/reviews/infrastructure/persistence/mongo/repositories/ReviewRepository.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/infrastructure/persistence/mongo/repositories/ReviewRepository.java
@@ -3,12 +3,14 @@ package pe.edu.upc.hardko.store.reviews.infrastructure.persistence.mongo.reposit
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.hardko.store.reviews.domain.model.aggregates.Review;
+import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.ProductId;
+import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.UserId;
 
 import java.util.List;
 
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
-    boolean existsReviewBy(String id);
-    List<Review> findReviewsByProductId (String productId);
-    List<Review> findReviewsByUserId(String userId);
+    boolean existsReviewById(String id);
+    List<Review> findReviewsByProductId (ProductId productId);
+    List<Review> findReviewsByUserId(UserId userId);
 }
