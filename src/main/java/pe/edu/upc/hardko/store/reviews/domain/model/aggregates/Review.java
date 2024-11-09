1e72da3d3c6989668e6cdb3d9c06280de6bba6b0
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:17:28 -0500

    feat(reviews): add List of users that liked the reviews.

--- src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/aggregates/Review.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/aggregates/Review.java
@@ -1,13 +1,18 @@
 package pe.edu.upc.hardko.store.reviews.domain.model.aggregates;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+import pe.edu.upc.hardko.store.reviews.domain.model.commands.CreateReviewCommand;
 import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.ProductId;
 import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.UserId;
 import pe.edu.upc.hardko.store.shared.domain.model.entities.AuditableModel;
+
+import java.util.List;
 
 @Getter
 @Document(collection = "reviews")
@@ -16,13 +21,14 @@ import pe.edu.upc.hardko.store.shared.domain.model.entities.AuditableModel;
 public class Review extends AuditableModel {
 
     @NotNull
-
     private UserId userId;
 
     @NotNull
     private ProductId productId;
 
     @NotNull
+    @Min(1)
+    @Max(5)
     private Integer rating;
 
     @NotNull
@@ -30,5 +36,32 @@ public class Review extends AuditableModel {
 
     @NotNull
     private String comment;
+
+    private List<UserId> likedBy;
+
+    public Review(String userId, String productId, Integer rating, String title, String comment) {
+        this.userId = new UserId(userId);
+        this.productId = new ProductId(productId);
+        this.rating = rating;
+        this.title = title;
+        this.comment = comment;
+        this.likedBy = List.of();
+    }
+
+    public Review(CreateReviewCommand command) {
+        this.userId = new UserId(command.userId());
+        this.productId = new ProductId(command.productId());
+        this.rating = command.rating();
+        this.title = command.title();
+        this.comment = command.comment();
+        this.likedBy = List.of();
+    }
+
+    public void addLike(UserId userId) {
+        if (!likedBy.contains(userId)) {
+            likedBy.add(userId);
+        }
+    }
+
 
 }
