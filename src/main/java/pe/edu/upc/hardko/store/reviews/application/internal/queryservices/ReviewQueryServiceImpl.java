1a664de853bf35cc597035e8c0e586415c032b68
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:14:38 -0500

    feat(reviews): implemented handle for get review by id.

--- src/main/java/pe/edu/upc/hardko/store/reviews/application/internal/queryservices/ReviewQueryServiceImpl.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/application/internal/queryservices/ReviewQueryServiceImpl.java
@@ -1,26 +1,30 @@
 package pe.edu.upc.hardko.store.reviews.application.internal.queryservices;
 
 import org.springframework.stereotype.Service;
-import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalProductService;
-import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalUserService;
+import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalReviewProductService;
+import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalReviewUserService;
 import pe.edu.upc.hardko.store.reviews.domain.model.aggregates.Review;
+import pe.edu.upc.hardko.store.reviews.domain.model.queries.GetReviewByIdQuery;
 import pe.edu.upc.hardko.store.reviews.domain.model.queries.GetReviewsByProductIdQuery;
 import pe.edu.upc.hardko.store.reviews.domain.model.queries.GetReviewsByUserIdQuery;
+import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.ProductId;
+import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.UserId;
 import pe.edu.upc.hardko.store.reviews.domain.services.ReviewQueryService;
 import pe.edu.upc.hardko.store.reviews.infrastructure.persistence.mongo.repositories.ReviewRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReviewQueryServiceImpl implements ReviewQueryService {
 
     private final ReviewRepository reviewRepository;
-    private final ExternalProductService externalProductService;
-    private final ExternalUserService externalUserService;
+    private final ExternalReviewProductService externalProductService;
+    private final ExternalReviewUserService externalUserService;
 
     public ReviewQueryServiceImpl(ReviewRepository reviewRepository,
-                                  ExternalProductService externalProductService,
-                                  ExternalUserService externalUserService) {
+                                  ExternalReviewProductService externalProductService,
+                                  ExternalReviewUserService externalUserService) {
         this.reviewRepository = reviewRepository;
         this.externalProductService = externalProductService;
         this.externalUserService = externalUserService;
@@ -29,13 +33,13 @@ public class ReviewQueryServiceImpl implements ReviewQueryService {
     @Override
     public List<Review> handle(GetReviewsByProductIdQuery query) {
 
-        var productId = query.productId().productId();
+        var productId = query.productId();
 
         if (!this.externalProductService.existsByProductId(productId)) {
             throw new IllegalArgumentException("Product not found");
         }
 
-        var reviews = this.reviewRepository.findReviewsByProductId(productId);
+        var reviews = this.reviewRepository.findReviewsByProductId(new ProductId(productId));
 
         return reviews;
     }
@@ -43,13 +47,18 @@ public class ReviewQueryServiceImpl implements ReviewQueryService {
     @Override
     public List<Review> handle(GetReviewsByUserIdQuery query) {
 
-        var userId = query.userId().userId();
+        var userId = query.userId();
 
         if(!this.externalProductService.existsByProductId(userId)){
             throw new IllegalArgumentException("User not found");
         }
 
-        var reviews = this.reviewRepository.findReviewsByUserId(userId);
+        var reviews = this.reviewRepository.findReviewsByUserId(new UserId(userId));
         return reviews;
+    }
+
+    @Override
+    public Optional<Review> handle(GetReviewByIdQuery query) {
+        return this.reviewRepository.findById(query.reviewId());
     }
 }
