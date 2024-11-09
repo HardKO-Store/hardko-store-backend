0c0b55bf0e0c1220dafae00829b18d790d30d377
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:15:01 -0500

    feat(reviews): add get reviews by user id query.

--- src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/queries/GetReviewsByUserIdQuery.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/queries/GetReviewsByUserIdQuery.java
@@ -1,6 +1,5 @@
 package pe.edu.upc.hardko.store.reviews.domain.model.queries;
 
-import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.UserId;
 
-public record GetReviewsByUserIdQuery(UserId userId) {
+public record GetReviewsByUserIdQuery(String userId) {
 }
