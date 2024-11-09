812439158bec69913d0ccd626b17bb83729eca34
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:15:43 -0500

    feat(reviews): add Add like to review by id command.

--- src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/commands/AddLikeToReviewByIdCommand.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/commands/AddLikeToReviewByIdCommand.java
@@ -2,5 +2,5 @@ package pe.edu.upc.hardko.store.reviews.domain.model.commands;
 
 import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.ProductId;
 
-public record AddLikeToReviewByIdCommand(ProductId productId) {
+public record AddLikeToReviewByIdCommand(String reviewId, String userId) {
 }
