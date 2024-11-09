92ed88bdb3d0fe7d9b31743875d43091ea2bd804
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:16:40 -0500

    feat(reviews): add assemblers for resources.

---
+++ src/main/java/pe/edu/upc/hardko/store/reviews/interfaces/rest/transform/ReviewResourceFromEntityAssembler.java
@@ -0,0 +1,18 @@
+package pe.edu.upc.hardko.store.reviews.interfaces.rest.transform;
+
+import pe.edu.upc.hardko.store.reviews.domain.model.aggregates.Review;
+import pe.edu.upc.hardko.store.reviews.interfaces.rest.resources.ReviewResource;
+
+public class ReviewResourceFromEntityAssembler {
+    public static ReviewResource toResourceFromEntity(Review entity){
+        return new ReviewResource(
+                entity.getId(),
+                entity.getProductId().productId(),
+                entity.getUserId().userId(),
+                entity.getRating(),
+                entity.getTitle(),
+                entity.getComment(),
+                entity.getLikedBy().size()
+        );
+    }
+}
