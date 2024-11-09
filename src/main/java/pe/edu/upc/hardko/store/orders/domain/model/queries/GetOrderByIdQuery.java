b1f643a89f62c21aaa175ad6f2776553512df60a
NeoRise456 <neorise456@gmail.com>
2024-11-09 17:17:18 -0500

    feat(orders): add queries for orders.

---
+++ src/main/java/pe/edu/upc/hardko/store/orders/domain/model/queries/GetOrderByIdQuery.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.hardko.store.orders.domain.model.queries;
+
+public record GetOrderByIdQuery(String orderId) {
+}
---
+++ src/main/java/pe/edu/upc/hardko/store/orders/domain/model/queries/GetOrdersByUserIdQuery.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.hardko.store.orders.domain.model.queries;
+
+public record GetOrdersByUserIdQuery(String userId) {
+}
