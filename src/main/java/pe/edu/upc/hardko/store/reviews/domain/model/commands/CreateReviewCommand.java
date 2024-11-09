c092a3f08df0625525645e20babd2d167c31a2b8
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:15:20 -0500

    feat(reviews): add create review command.

--- src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/commands/CreateReviewCommand.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/commands/CreateReviewCommand.java
@@ -1,4 +1,10 @@
 package pe.edu.upc.hardko.store.reviews.domain.model.commands;
 
-public record CreateReviewCommand() {
+public record CreateReviewCommand(
+        String productId,
+        String userId,
+        Integer rating,
+        String title,
+        String comment
+) {
 }
