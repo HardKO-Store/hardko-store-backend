a43aff6fe609785f9f8a3515460ecc5a5da7bd6d
NeoRise456 <neorise456@gmail.com>
2024-11-09 01:13:46 -0500

    feat(reviews): implemented command service.

--- src/main/java/pe/edu/upc/hardko/store/reviews/application/internal/commandservices/ReviewCommandServiceImpl.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/application/internal/commandservices/ReviewCommandServiceImpl.java
@@ -1,12 +1,13 @@
 package pe.edu.upc.hardko.store.reviews.application.internal.commandservices;
 
 import org.springframework.stereotype.Service;
-import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalProductService;
-import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalUserService;
+import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalReviewProductService;
+import pe.edu.upc.hardko.store.reviews.application.internal.outboundservices.ExternalReviewUserService;
 import pe.edu.upc.hardko.store.reviews.domain.model.aggregates.Review;
 import pe.edu.upc.hardko.store.reviews.domain.model.commands.AddLikeToReviewByIdCommand;
 import pe.edu.upc.hardko.store.reviews.domain.model.commands.CreateReviewCommand;
 import pe.edu.upc.hardko.store.reviews.domain.model.commands.DeleteReviewCommand;
+import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.UserId;
 import pe.edu.upc.hardko.store.reviews.domain.services.ReviewCommandService;
 import pe.edu.upc.hardko.store.reviews.infrastructure.persistence.mongo.repositories.ReviewRepository;
 
@@ -16,12 +17,12 @@ import java.util.Optional;
 public class ReviewCommandServiceImpl implements ReviewCommandService {
 
     private final ReviewRepository reviewRepository;
-    private final ExternalProductService externalProductService;
-    private final ExternalUserService externalUserService;
+    private final ExternalReviewProductService externalProductService;
+    private final ExternalReviewUserService externalUserService;
 
     public ReviewCommandServiceImpl(ReviewRepository reviewRepository,
-                                  ExternalProductService externalProductService,
-                                  ExternalUserService externalUserService) {
+                                  ExternalReviewProductService externalProductService,
+                                  ExternalReviewUserService externalUserService) {
         this.reviewRepository = reviewRepository;
         this.externalProductService = externalProductService;
         this.externalUserService = externalUserService;
@@ -29,16 +30,51 @@ public class ReviewCommandServiceImpl implements ReviewCommandService {
 
     @Override
     public Optional<Review> handle(CreateReviewCommand command) {
-        return Optional.empty();
+        if(!this.externalUserService.existsByUserId( (command.userId()) )){
+            throw new IllegalArgumentException("User not found");
+        }
+        if(!this.externalProductService.existsByProductId( (command.productId()) )){
+            throw new IllegalArgumentException("Product not found");
+        }
+
+        var review = new Review(command);
+
+        try {
+            this.reviewRepository.save(review);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while saving review: " + e.getMessage());
+        }
+
+        return Optional.of(review);
     }
 
     @Override
     public void handle(DeleteReviewCommand command) {
+        if (!this.reviewRepository.existsById(command.reviewId())) {
+            throw new IllegalArgumentException("Review with id " + command.reviewId() + " does not exist");
+        }
 
+        try {
+            this.reviewRepository.deleteById(command.reviewId());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while deleting review: " + e.getMessage());
+        }
     }
 
     @Override
-    public Boolean handle(AddLikeToReviewByIdCommand command) {
-        return null;
+    public void handle(AddLikeToReviewByIdCommand command) {
+        if (!this.reviewRepository.existsById(command.reviewId())) {
+            throw new IllegalArgumentException("Review with id " + command.reviewId() + " does not exist");
+        }
+
+        var review = this.reviewRepository.findById(command.reviewId()).get();
+        review.addLike(new UserId(command.userId()));
+
+        try {
+            this.reviewRepository.save(review);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while saving review: " + e.getMessage());
+        }
+
     }
 }
