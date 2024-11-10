485cf18bae810d28f131372e95df853a3908e4e6
NeoRise456 <neorise456@gmail.com>
2024-11-09 21:30:57 -0500

    feat(orders): add get user delivery address external user service.

--- src/main/java/pe/edu/upc/hardko/store/orders/application/internal/outboundservices/ExternalOrderUserService.java
+++ src/main/java/pe/edu/upc/hardko/store/orders/application/internal/outboundservices/ExternalOrderUserService.java
@@ -2,6 +2,7 @@ package pe.edu.upc.hardko.store.orders.application.internal.outboundservices;
 
 import org.springframework.stereotype.Service;
 import pe.edu.upc.hardko.store.IAM.interfaces.acl.UserContextFacade;
+import pe.edu.upc.hardko.store.orders.domain.model.valueobjects.DeliveryAddress;
 
 @Service
 public class ExternalOrderUserService {
@@ -15,5 +16,20 @@ public class ExternalOrderUserService {
     public boolean existsUserById(String userId) {
         return this.userContextFacade.existsUserById(userId);
     }
+
+    public DeliveryAddress getUserAddressById(String userId) {
+
+        var userAddress = this.userContextFacade.GetUserAddressById(userId).get();
+
+        var deliveryAddress = new DeliveryAddress(
+                userAddress.getCountry(),
+                userAddress.getCity(),
+                userAddress.getStreet(),
+                userAddress.getZip()
+        );
+
+        return deliveryAddress;
+    }
+
 
 }
