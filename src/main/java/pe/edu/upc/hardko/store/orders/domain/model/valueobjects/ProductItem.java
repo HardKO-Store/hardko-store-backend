325c5cb9f7ca27240db359a38d606482b038ef6f
NeoRise456 <neorise456@gmail.com>
2024-11-09 23:07:38 -0500

    refactor(orders): changed parameters declaration for product item.

--- src/main/java/pe/edu/upc/hardko/store/orders/domain/model/valueobjects/ProductItem.java
+++ src/main/java/pe/edu/upc/hardko/store/orders/domain/model/valueobjects/ProductItem.java
@@ -1,8 +1,10 @@
 package pe.edu.upc.hardko.store.orders.domain.model.valueobjects;
 
-public record ProductItem(String ProductId, Integer quantity) {
+//TODO: change monse
+
+public record ProductItem(String productId, Integer quantity) {
     public ProductItem {
-        if (ProductId == null || ProductId.isBlank()) {
+        if (productId == null || productId.isBlank()) {
             throw new IllegalArgumentException("Product Id cannot be null or empty");
         }
         if (quantity == null || quantity <= 0) {
