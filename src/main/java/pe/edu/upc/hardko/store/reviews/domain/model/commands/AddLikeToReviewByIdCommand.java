bd47578df06769d0e74f2dbcd7501ee457cc0977
NeoRise456 <neorise456@gmail.com>
2024-11-09 22:24:23 -0500

    refactor(reviews): deleted product id import.

--- src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/commands/AddLikeToReviewByIdCommand.java
+++ src/main/java/pe/edu/upc/hardko/store/reviews/domain/model/commands/AddLikeToReviewByIdCommand.java
@@ -1,6 +1,4 @@
 package pe.edu.upc.hardko.store.reviews.domain.model.commands;
 
-import pe.edu.upc.hardko.store.reviews.domain.model.valueobjects.ProductId;
-
 public record AddLikeToReviewByIdCommand(String reviewId, String userId) {
 }
